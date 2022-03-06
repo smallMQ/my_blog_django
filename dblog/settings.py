877888ffd88b40de48835946cbc1ae4c5de5cc4c
MQ <2271443486@qq.com>
2022-03-06 19:27:37 +0800

    ok

--- dblog/settings.py
+++ dblog/settings.py
@@ -88,8 +88,8 @@ DATABASES = {
             'default': {
                 'ENGINE': 'django.db.backends.mysql',
                 'NAME': 'new_blog',
-                'USER':'root',
-                'PASSWORD':'smallmq',
+                'USER':'smallmq',
+                'PASSWORD':'wangmengqi123',
                 'HOST':'127.0.0.1',
                 'PORT':3306
 
